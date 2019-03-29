753718c94360e516dfb0e2c66a6cfabd75265b21
Dominique Rochefort <dominique.rochefort@hmhco.com>
2019-03-29 12:55:31 -0400

    add z

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { MyClass } from './my-class';
 
-export function sum(x: number, y: number): number {
-    return x + y;
+export function sum(x: number, y: number, z: number): number {
+    return x + y+ z;
 }
+
